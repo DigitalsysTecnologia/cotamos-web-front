86ccc79df98c50e54b7292aa5b23d03335664a93
Eduardo Andrade <eduardo@andrade.com>
2018-05-01 22:46:58 -0300

    Implementing areaCode Validation

--- src/scenes/health-insurance/Index.vue
+++ src/scenes/health-insurance/Index.vue
@@ -347,7 +347,16 @@ export default {
       return Validator.value(value)
         .required("Por favor, nos informe seu DDD.")
         .digit("DDD com valor não-numérico")
-        .minLength(2, "Favor Informar o DDD com 2 dígitos");
+        .minLength(2, "Favor Informar o DDD com 2 dígitos")
+        .custom(function() {
+            const validAreaCodes = [11,12,13,14,15,16,17,18,19,21,22,24,27,28,31,32,33,34,35,37,38,41,42,43,44,45,46,47,48,49,51,53,54,55,61,62,63,64,65,66,67,68,69,71,73,74,75,77,79,81,82,83,84,85,86,87,88,89,91,92,93,94,95,96,97,98,99]
+            const ddd = parseInt(value)
+            if(!validAreaCodes.includes(ddd)) {
+              return "DDD inválido";
+            }
+
+            return null;
+        });
     },
     "proposal.proposer.phones.0.number": function(value) {
       const val = value.replace(/\D/g, "");
