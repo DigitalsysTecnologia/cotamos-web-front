f56ed346f8eafad9abc346f81cba535467ca3f08
Eduardo Andrade <eduardo@andrade.com>
2018-05-01 21:21:41 -0300

    Including company document (CNPJ) and SUSEP on Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,6 +14,13 @@
                             <a href="https://www.facebook.com/Cotamos-168236910394093"><i class="fa fa-twitter" aria-hidden="true"/></a>
                         </p>
                         <p class="direitos-rdp">Todos os direitos reservados.</p>
+                        <p class="informacoes-legais"> 
+                            CNPJ 30.249.014/0001-04
+                        </p>
+                        <p class="informacoes-legais"> 
+                            SUSEP 20.2021328.0
+                        </p>
+                        
                 </div>
             </div>
         </div>
@@ -31,5 +38,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.informacoes-legais {
+    text-align: center;
+    color: #fff;
+    margin: 0px;
+}
 </style>
