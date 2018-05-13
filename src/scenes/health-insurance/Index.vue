2ee0de64de8a0e8c369d4bd63964e4404db0d416
Eduardo Andrade <eduardo.andrade@cotamos.com>
2018-05-12 23:44:36 -0300

    Fix name rules

--- src/scenes/health-insurance/Index.vue
+++ src/scenes/health-insurance/Index.vue
@@ -206,7 +206,6 @@ export default {
           return;
         }
       } catch (err) {
-        
         return;
       }
 
@@ -275,7 +274,13 @@ export default {
     "proposal.proposer.name": function(value) {
       return Validator.value(value)
         .required("Por favor, nos informe o seu nome.")
-        .minLength(5, "Por favor, nos informe o seu nome.");
+        .minLength(5, "Por favor, nos informe o seu nome.")
+        .custom(function() {
+          if ((value.split(" ").length - 1) < 1) {
+            return "Por favor, nos informe o seu nome."
+          }
+          
+        });
     },
     "proposal.proposer.email": function(value) {
       return Validator.value(value)
@@ -296,13 +301,11 @@ export default {
         });
     },
     "proposal.proposer.homeAddress.state": function(value) {
-      
       return Validator.value(value)
         .required("Por favor, nos informe o seu estado.")
         .minLength(2, "Estado Inválido");
     },
     "proposal.proposer.homeAddress.city": function(value) {
-      
       return Validator.value(value).required(
         "Por favor, nos informe a sua cidade."
       );
@@ -349,13 +352,81 @@ export default {
         .digit("DDD com valor não-numérico")
         .minLength(2, "Favor Informar o DDD com 2 dígitos")
         .custom(function() {
-            const validAreaCodes = [11,12,13,14,15,16,17,18,19,21,22,24,27,28,31,32,33,34,35,37,38,41,42,43,44,45,46,47,48,49,51,53,54,55,61,62,63,64,65,66,67,68,69,71,73,74,75,77,79,81,82,83,84,85,86,87,88,89,91,92,93,94,95,96,97,98,99]
-            const ddd = parseInt(value)
-            if(!validAreaCodes.includes(ddd)) {
-              return "DDD inválido";
-            }
+          const validAreaCodes = [
+            11,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17,
+            18,
+            19,
+            21,
+            22,
+            24,
+            27,
+            28,
+            31,
+            32,
+            33,
+            34,
+            35,
+            37,
+            38,
+            41,
+            42,
+            43,
+            44,
+            45,
+            46,
+            47,
+            48,
+            49,
+            51,
+            53,
+            54,
+            55,
+            61,
+            62,
+            63,
+            64,
+            65,
+            66,
+            67,
+            68,
+            69,
+            71,
+            73,
+            74,
+            75,
+            77,
+            79,
+            81,
+            82,
+            83,
+            84,
+            85,
+            86,
+            87,
+            88,
+            89,
+            91,
+            92,
+            93,
+            94,
+            95,
+            96,
+            97,
+            98,
+            99
+          ];
+          const ddd = parseInt(value);
+          if (!validAreaCodes.includes(ddd)) {
+            return "DDD inválido";
+          }
 
-            return null;
+          return null;
         });
     },
     "proposal.proposer.phones.0.number": function(value) {
