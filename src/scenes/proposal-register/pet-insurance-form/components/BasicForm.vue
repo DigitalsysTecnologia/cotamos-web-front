c74d324ba6293079dd082cb1011a9940c0abb94c
Eduardo Andrade <eduardo.andrade@cotamos.com>
2018-06-20 23:25:40 -0300

    New fields

--- src/scenes/proposal-register/pet-insurance-form/components/BasicForm.vue
+++ src/scenes/proposal-register/pet-insurance-form/components/BasicForm.vue
@@ -2,7 +2,7 @@
   <div>
     <h3>Primeiro, vamos verificar a disponibilidade para sua região, tá bom?</h3>
     <h3>Para isso, vamos precisar de algumas informações</h3>
-    <div class="col-xs-12 col-sm-6">
+    <div class="col-xs-12 col-sm-12">
       <FormInput label="Nome do seu pet"
                   icon="fa-paw" 
                   id="proposal.petInsuranceData.name"
@@ -29,6 +29,35 @@
                   v-model.trim="proposal.proposer.homeAddress.zipCode" />
     </div>
 
+    <div class="col-sm-12 col-xs-12">
+      <FormInput  label="E-mail" 
+                  icon="fa-at" 
+                  id="proposal.proposer.email"
+                  :validationMessage="validation.firstError('proposal.proposer.email')"
+                  v-model.trim="proposal.proposer.email" />
+    </div>
+
+    <div class="col-md-2 col-sm-3 col-xs-12">
+        <FormInput  label="DDD" 
+                    icon="fa-phone" 
+                    type="text"
+                    id="proposal.proposer.phones.0.areaCode"
+                    mask="##"
+                    :validationMessage="validation.firstError('proposal.proposer.phones.0.areaCode')"
+                    v-model.trim="proposal.proposer.phones[0].areaCode" />
+    </div>
+
+    <div class="col-sm-9 col-md-10 col-xs-12 offset-md-1 offset-sm-1">
+        <FormInput label="Celular ou Telefone" 
+                    icon="fa-phone" 
+                    id="proposal.proposer.phones.0.number"
+                    maxLength="15"
+                    :mask="getPhoneMask(proposal.proposer.phones[0].number)"
+                    type="text"
+                    :validationMessage="validation.firstError('proposal.proposer.phones.0.number')"
+                    v-model.trim="proposal.proposer.phones[0].number" />
+    </div>
+
     <div class="col-xs-12">
       <CallToAction className='pull-left' v-on:click="submitProposal">
         Próximo &nbsp;
@@ -44,7 +73,7 @@ import FormInput from "@/components/FormInput";
 import FormSelect from "@/components/FormSelect.vue";
 import validator from "@/utils/validator";
 import factory from "@/utils/factory";
-import CallToAction from "@/components/CallToAction.vue"
+import CallToAction from "@/components/CallToAction.vue";
 
 export default {
   name: "BasicPetInsuranceForm",
@@ -60,17 +89,37 @@ export default {
       if (isValid) {
         this.$emit("next", null);
       }
+    },
+    getPhoneMask: function(phone) {
+      let phoneNumber = null;
+      if (phone) {
+        phoneNumber = phone.replace(/\D/g, "");
+      } else {
+        phoneNumber = "";
+      }
+
+      if (phoneNumber.length <= 8) {
+        return "####-####";
+      } else {
+        return "#####-####";
+      }
     }
   },
   validators: {
     "proposal.petInsuranceData.name": value => validator.validatePetName(value),
-    "proposal.proposer.homeAddress.zipCode": value =>  validator.validateZipCode(value),
-    "proposal.petInsuranceData.age": value => validator.validatePetAge(value)
+    "proposal.proposer.homeAddress.zipCode": value =>
+      validator.validateZipCode(value),
+    "proposal.petInsuranceData.age": value => validator.validatePetAge(value),
+    "proposal.proposer.email": value => validator.validateEmail(value),
+    "proposal.proposer.phones.0.areaCode": value =>
+      validator.validatePhoneAreaCode(value),
+    "proposal.proposer.phones.0.number": value =>
+      validator.validatePhoneNumber(value)
   },
   components: {
     FormInput: FormInput,
     FormSelect: FormSelect,
-    CallToAction:CallToAction
+    CallToAction: CallToAction
   }
 };
 </script>
