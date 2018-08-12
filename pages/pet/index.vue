41e892ad3eb7cdaeb6a8106d13160b72abc196c9
Eduardo Andrade <eduardo.andrade@cotamos.com>
2018-08-12 18:51:16 -0300

    Using SSR

---
+++ pages/pet/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Pet",
+  beforeMount() {
+    this.$router.push({
+      path: "/plano-de-saude-pet",
+      query: {
+        utm_source: "flyer",
+        utm_campaign: "pet_flyer"
+      }
+    });
+  },
+  components: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
