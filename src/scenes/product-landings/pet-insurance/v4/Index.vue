4ab5dd4a68dca044f3c4b6043186b89c8d35b487
Eduardo Andrade <eduardo.andrade@cotamos.com>
2018-08-12 01:33:54 -0300

    New meta-tags on Pet Insurance

--- src/scenes/product-landings/pet-insurance/v4/Index.vue
+++ src/scenes/product-landings/pet-insurance/v4/Index.vue
@@ -196,33 +196,6 @@ export default {
   data() {
     return {};
   },
-
-  head: {
-     title: {
-      inner: 'It will be a pleasure'
-    },
-    // Meta tags
-    meta: [
-      { name: 'application-name', content: 'Name of my application' },
-      { name: 'description', content: 'A description of the page', id: 'desc' }, // id to replace intead of create element
-      // ...
-      // Twitter
-      { name: 'twitter:title', content: 'Content Title' },
-      // with shorthand
-      { n: 'twitter:description', c: 'Content description less than 200 characters'},
-      // ...
-      // Google+ / Schema.org
-      { itemprop: 'name', content: 'Content Title' },
-      { itemprop: 'description', content: 'Content Title' },
-      // ...
-      // Facebook / Open Graph
-      { property: 'fb:app_id', content: '123456789' },
-      { property: 'og:title', content: 'Content Title' },
-      // with shorthand
-      { p: 'og:image', c: 'https://example.com/image.jpg' },
-      // ...
-    ],
-  },
   async beforeMount() {
     router = this.$router;
     utils.saveLandingQueryParams(this.$route.query);
