7311d53bdec8b2b4865dec44b2cc98f86a9064d8
Eduardo Andrade <eduardo.luminy@gmail.com>
2018-12-24 01:12:23 +0000

    Feature/kubernetes

--- utils/apiClient.js
+++ utils/apiClient.js
@@ -11,10 +11,10 @@ function getUrl() {
       // return 'http://localhost:8080/api/v1'
     }
     else if (window.location.host.indexOf('homolog.cotamos.com') != -1) {
-      return 'https://backend-homolog.cotamos.com/gateway/api/v1'
+      return 'https://backend-homolog.cotamos.com/api/v1'
     }
     else {
-      return 'https://backend.cotamos.com/gateway/api/v1'
+      return 'https://backend.cotamos.com/api/v1'
     }
   }
   return process.env.baseUrl
