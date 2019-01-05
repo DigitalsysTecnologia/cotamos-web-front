4171e292d23131dc3afb44bb825f4a15e28ed113
Eduardo Andrade <eduardo@example.com>
2019-01-05 18:21:02 -0200

    Fix Current Date

--- pages/seja-parceiro/index.vue
+++ pages/seja-parceiro/index.vue
@@ -50,7 +50,7 @@
       </v-flex>
 
       <v-flex xs12 class="section">
-        <h2 class="subtitle text-center">Perguntas Frequentes</h2>
+        <h2 class="subtitle text-center">Possíveis Dúvidas</h2>
         <Faq :items="faqs"/>
       </v-flex>
 
