4d0365210b87cc0d657ca2621b4934ff88094832
Eduardo Andrade <eduardo@andrade.com>
2019-04-14 14:29:47 -0300

    Including health plan white label

--- layouts/wl/a3/default.vue
+++ layouts/wl/a3/default.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
-    <Header logoImage="/img/wl/a3/logo.png"/>
+    <!-- Header -->
+    <nav class="navbar is-transparent is-fixed-top" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/wl/a3/plano-de-saude">
+          <img src="/img/wl/a3/logo.png" width="112" height="28" class="logo-image">
+        </a>
+      </div>
+    </nav>
+    <!-- /Header -->
+
     <nuxt/>
-    <Footer :hideLogo="true"/>
+
+    <!-- Footer -->
+    <footer class="footer">
+      <div class="content has-text-centered has-text-light is-half is-offset-one-quarter">
+        <p>
+          <img src="/img/wl/a3/a3-tipografia-branco.png" style="width:200px;">
+        </p>
+        <p>
+          <span>CNPJ 30.249.014/0001-04</span>
+          <br>
+          <span>SUSEP 20.2021328.0</span>
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -23,4 +45,7 @@ export default {
 </script>
 
 <style>
+.footer {
+  background: #f3ba51;
+}
 </style>
